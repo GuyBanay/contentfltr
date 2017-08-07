5ce7d72011ad7baffe7d9889e0419a425f4bad36
GuyBanay <gbanay@gmail.com>
2017-08-07 15:01:35 +0300

    Disposing RuleManager in some tests

--- ContentFilter/ContentFilter.Tests/RulesShould.cs
+++ ContentFilter/ContentFilter.Tests/RulesShould.cs
@@ -3,10 +3,10 @@ using Xunit;
 
 namespace ContentFilter.Tests
 {
-    public class RulesShould: RuleTestBase
+    public class RulesShould : RuleTestBase
     {
         [Theory]
-        [InlineData("R1","Hello bla bla",true)]
+        [InlineData("R1", "Hello bla bla", true)]
         [InlineData("R1", "bla bla Hello bla bla", false)]
         [InlineData("R2", "Hello bla bla bye", true)]
         [InlineData("R2", "bla bla Hello bla bla", false)]
@@ -24,51 +24,52 @@ namespace ContentFilter.Tests
         [InlineData("R7", "bla bla worlD bla bla bye jhg kjhg kjhg kjhg jkhgkj", false)]
         public void TestExistsRules(string ruleName, string line, bool expectedResult)
         {
-            var ruleManager = GetRuleManagerWithRules();
-            var isMatch = ruleManager.GetRule(ruleName).IsMatch(line);
-            Assert.Equal(expectedResult,isMatch);
+            using (var ruleManager = GetRuleManagerWithRules())
+            {
+                var isMatch = ruleManager.GetRule(ruleName).IsMatch(line);
+                Assert.Equal(expectedResult, isMatch);
+            }
         }
 
         [Fact]
         public void TestNotExistsRules()
         {
-            var ruleManager = GetRuleManagerWithRules();
-            const string notExistsRuleName = "R17";
-            var expectedException = new Exception($"Cannot find rule {notExistsRuleName}");
-            var actuaException = new Exception();
-            try
+            using (var ruleManager = GetRuleManagerWithRules())
             {
-                ruleManager.GetRule(notExistsRuleName).IsMatch("line");
+                const string notExistsRuleName = "R17";
+                var expectedException = new Exception($"Cannot find rule {notExistsRuleName}");
+                var actuaException = new Exception();
+                try
+                {
+                    ruleManager.GetRule(notExistsRuleName).IsMatch("line");
+                }
+                catch (Exception e)
+                {
+                    actuaException = e;
+                }
+                Assert.Equal(expectedException.Message, actuaException.Message);
             }
-            catch (Exception e)
-            {
-                actuaException = e;
-            }
-            Assert.Equal(expectedException.Message, actuaException.Message);
         }
 
 
         [Fact]
         public void AddSameRuleNameTwice()
         {
-            var ruleManager = GetRuleManagerWithRules();
             const string existsRuleName = "R2";
-
-            var expectedException = new Exception($"Rule already exists {existsRuleName}");
-            var actuaException = new Exception();
-            try
+            using (var ruleManager = GetRuleManagerWithRules())
             {
-                ruleManager.AddRule(existsRuleName, "XOR", "R1 R3");
+                var expectedException = new Exception($"Rule already exists {existsRuleName}");
+                var actuaException = new Exception();
+                try
+                {
+                    ruleManager.AddRule(existsRuleName, "XOR", "R1 R3");
+                }
+                catch (Exception e)
+                {
+                    actuaException = e;
+                }
+                Assert.Equal(expectedException.Message, actuaException.Message);
             }
-            catch (Exception e)
-            {
-                actuaException = e;
-            }
-            Assert.Equal(expectedException.Message, actuaException.Message);
         }
-
-        
-
-       
     }
 }
